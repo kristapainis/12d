da1c6a45247b06ddf8d85cffc20f6ce1df4a4f3b
Kristaps Smilga <98461072+kristapainis@users.noreply.github.com>
2022-01-26 12:30:56 +0000

    pievienoju daļu no risinājuma

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import math
+"""import math
 mala=int(input("Ievadi kvadrāta malas garumu centimetros: "))
 laukums1=mala*mala
 laukums2=(mala+5)*(mala+5)
 summa=laukums2/laukums1
 atskiriba=summa*100
 rezultats=math.ceil(atskiriba)
-print(f'Kvadrāta laukums mainās par {rezultats}%')
+print(f'Kvadrāta laukums mainās par {rezultats}%')"""
+
+radiuss=float(input("Ievadi riņķa rādiusu: "))
+hipotenuza=int(input("Ievadi trijstūra hipotenūzas garumu: "))
+laukums1=math.pi*(radiuss**2)
